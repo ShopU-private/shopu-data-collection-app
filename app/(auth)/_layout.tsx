c2428d59328cf4b3f71ac294744e2bdc1ea0ec54
ayush931 <ayushkumar9315983@gmail.com>
2026-01-19 20:47:38 +0530

    changed

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -5,7 +5,6 @@ const AuthLayout = () => {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name='login' />
-      <Stack.Screen name='logged' />
     </Stack>
   )
 }
